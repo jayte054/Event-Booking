7fe1c7fee55793e8e15318e60127f1e684b881e0
jayte054 <54476858+jayte054@users.noreply.github.com>
2024-01-17 08:38:25 +0100

    query update

--- index.js
+++ index.js
@@ -10,7 +10,34 @@ const User = require("./models/user.model")
 const app = express();
 app.use(bodyParser.json());
 
-const eventListings = []
+const events = async (eventIds) => {
+    const response = await Event.find({_id: { $in: eventIds}})
+    try {
+        return response.map(event => {
+            return {...event._doc, 
+                    _id: event.id, 
+                    creator: theUser.bind(this, event.creator)
+                }
+        })
+    } catch(error) {
+        console.log(error)
+        throw error
+    }
+}
+
+const theUser = async (userId) => {
+    const response = await User.findById(userId)
+    try{
+            return {...response._doc, 
+                    _id: response.id,
+                    createdEvents: events.bind(this, response._doc.createdEvents)
+                }
+    }catch(error) {
+        console.log(error) 
+    }
+    
+} 
+
 
 const schema = buildSchema(`
 
@@ -19,13 +46,15 @@ const schema = buildSchema(`
         title: String!,
         description: String!,
         price: Float!,
-        date: String!
+        date: String!,
+        creator: User!
     }
 
     type User {
         id: ID!,
         email: String!,
-        password: String
+        password: String,
+        createdEvents: [Event!]!
        }
 
     input EventInput {
@@ -42,7 +71,7 @@ const schema = buildSchema(`
 
     type RootQuery {
         getEvents: [Event!]!,
-        getSingleEvent: [Event!]!
+        getUsers: [User!]!
     }
 
     type RootMutation {
@@ -61,22 +90,16 @@ const rootValue = {
     getEvents: async () => {
         try{
            const result =  await Event.find()
-            return result
+            return result.map(event => {
+                return {...event._doc, creator: theUser.bind(this, event._doc.creator)}
+            })
         } catch(error) {
             console.log(error)
             throw error
         }
-    },
-
-    // getSingleEvent: async () => {
-    //     try{
-    //         await Event.find()
-    //     } catch(error) {
-    //         console.log(error)
-    //     }
-    // }
+    }, 
     
-    createEvent: ( args) => {
+    createEvent: async ( args) => {
         // const event = {
         //     id: Math.random().toString(),
         //     title: args.EventInput.title,
@@ -87,16 +110,39 @@ const rootValue = {
             title: args.EventInput.title,
             description: args.EventInput.description,
             price: args.EventInput.price,
-            date:  new Date(args.EventInput.date)
+            date:  new Date(args.EventInput.date),
+            creator: "65a661fab0dea001a287f9eb"
         });
         try {
+
+            const eventUser = async (user) => {
+                user = await User.findById("65a661fab0dea001a287f9eb")
+                if(!user) {
+                    throw new Error("user does not exist")
+                }
+                console.log(user)
+                user.createdEvents.push(event)
+                return user.save()
+            }
+            eventUser()
             event.save()
-            // return event;
-            return {...event._doc}
+            
+            return event;
+            // return {...event._doc}
         } catch(error) {
             console.log(error);
             throw error
         }  
+    },
+
+    getUsers: async () => {
+        const result = await User.find().populate("createdEvents")
+        try{
+            return result
+        } catch(error) {
+            console.log(error)
+            throw error
+        }
     },
 
     createUser: async (args) => {
